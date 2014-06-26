e9e0548b11e3d557b2870295801591ba546d5d28
Tom <Tom@Airi-Petteri-2.local>
2014-06-27 02:41:52 +0300

    Menu structuring, bug fixes, usage instructions, mockups, etc.

--- js/main.js
+++ js/main.js
@@ -1,29 +1,48 @@
+///////////////
+// FUNCTIONS //
+///////////////
+
+function scrollToAnchor(aid)
+{
+  var aTag = $("h1[name='"+ aid +"']");
+  $('html,body').animate({scrollTop: aTag.offset().top - 55},'fast');
+  // <span onclick="scrollToAnchor('foobar');">Foobar</span> links to <h1 name="foobar">Foobar</a>
+}
+
+///////////
+// LOGIC //
+///////////
+
 $(window).scroll(function()
+{
+  
+  // Home section's parallax effect
+  
+  if ($(window).width() > 1000)
   {
-    
-    // Home section's parallax effect
-    
-    if ($(window).width() > 1000)
-    {
-      var scrollPos = $(document).scrollTop();
-      $("#bg").css("top", -scrollPos/3+"px");
-    }
-    
-    var horizontalNaviHeight = 100;
-    var distanceToPageTop = $(document).scrollTop();
-    var homeSectionHeight = $("#home").height();
-    var toggleNaviFixValue = distanceToPageTop - homeSectionHeight + horizontalNaviHeight;
-    console.log(toggleNaviFixValue);
-    if (toggleNaviFixValue > 1)
-    {
-      $("#desktop-navi").css("position", "fixed");
-      $("#desktop-navi").css("bottom", "auto");
-      $("#desktop-navi").css("top", "100px");
-    }
-    else
-    {
-      $("#desktop-navi").css("position", "static");
-      $("#desktop-navi").css("margin-top", "-100px");
-    }
-    
-  }); 
+    var scrollPos = $(document).scrollTop();
+    $("#bg").css("top", -scrollPos/3+"px");
+  }
+  
+  var horizontalNaviHeight = 50;
+  var distanceToPageTop = $(document).scrollTop();
+  var homeSectionHeight = $("#home").height();
+  var toggleNaviFixValue = distanceToPageTop - homeSectionHeight + horizontalNaviHeight;
+  console.log(toggleNaviFixValue);
+  if (toggleNaviFixValue > 1)
+  {
+    $("#desktop-nav").css("position", "fixed");
+    $("#desktop-nav").css("bottom", "auto");
+    $("#desktop-nav").css("top", horizontalNaviHeight+"px");
+  }
+  else
+  {
+    $("#desktop-nav").css("position", "static");
+    $("#desktop-nav").css("margin-top", -horizontalNaviHeight+"px");
+  }
+  
+}); 
+
+$(document).ready(function(){
+   // foobar
+});
