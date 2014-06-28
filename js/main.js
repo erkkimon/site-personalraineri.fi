c6266c87f6c49254cc62afc445dd53471eaf9f0d
Tom <Tom@Airi-Petteri-2.local>
2014-06-29 00:31:25 +0300

    Facebook like button added w/ dynamic-like-button-switch and like-button-blinker.

--- js/main.js
+++ js/main.js
@@ -43,6 +43,15 @@ $(window).scroll(function()
   
 }); 
 
-$(document).ready(function(){
-   // foobar
+$(document).ready(function()
+{
+   // Load desired Facebook like button dynamically
+   dynamicLikeButtonSwitcher("https://www.facebook.com/personaltravis");
+   
+   // Blink Facebook like button
+   setInterval(function()
+   {
+     $(".likebutton-placeholder").animate({ "opacity": "0.2" }, "slow" );
+     $(".likebutton-placeholder").animate({ "opacity": "1.0" }, "slow" );
+   }, 15000);
 });
